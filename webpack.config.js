127c1f600af2b1e2f5b4c233575897d98301ec02
zarataZarata <tatjana.verchenko@gmail.com>
2019-08-22 21:35:08 +0300

    v1

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require('path');
 
 module.exports = {
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
